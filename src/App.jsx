4cf9a7d7707bce2f973035604cc6ca8ae022c187
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-06-24 15:24:30 -0300

    FIX: Correcting details

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ function App() {
   return (
     <>
       <NavBar/>
-      <HeroSection/>
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection/>
+      </div>
+     
     </>
   )
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -23,11 +23,11 @@ export default function HeroSection() {
         </div>
         
         <div className="flex mt-10 justify-center">
-            <video autoPlay loop muted className="rounded-lg w-1/3 border border-orange-700 shadow-orange-400 mx-2 my-4">
+            <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
                 <source src={video1} type="video/mp4"/>
                 Your browser does not support the video tag.
             </video>
-            <video autoPlay loop muted className="rounded-lg w-1/3 border border-orange-700 shadow-orange-400 mx-2 my-4">
+            <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
                 <source src={video2} type="video/mp4"/>
                 Your browser does not support the video tag.
             </video>
