6f42a46ce18a3eda7857ea04885155ae45164684
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-06-24 15:01:06 -0300

    FEAT: Creating HeroSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import HeroSection from "./components/HeroSection"
 import NavBar from "./components/NavBar"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <NavBar/>
+      <HeroSection/>
     </>
   )
 }
